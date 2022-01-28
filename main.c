145cb761170a2dbda88a7e35a0ef3f5f8070fad3
Machanhry <rebeccahuang2015@outlook.com>
2022-01-28 16:51:25 +0800

    1. Variables and constants.

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#define MY_AGE 10000
 
+const int MY_AGE1 = 10000;
 int main() {
-
+#
     int a = 10;
     a = 11;
 
-    printf("Hello, %d \n",a);
+//    printf("Hello, %d \n",a);
+    printf("My age is: %d\n", MY_AGE1);
+
     return 0;
 }
-
